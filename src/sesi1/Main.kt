abde4056e6f3f7179921194d8f91cb83e6d428bc
ilhamsuaib <ilhamsuaib10@gmail.com>
2018-11-19 21:31:01 +0700

    sesi 1 : Tipe data dan variabel

---
+++ src/sesi1/Main.kt
@@ -0,0 +1,35 @@
+package sesi1
+
+import java.util.*
+
+/**deklarasi dan inisialisasi variabel*/
+val nama: String = "Ilham"
+val usia: Int = 22
+val beratBadan: Double = 48.5
+val tinggiBadan: Int = 160
+val isStudent: Boolean = false
+val pekerjaan: Array<String> = arrayOf("Programmer", "Tukang", "Gamer", "dll")
+val nilaiPerSemester: Array<Int> = arrayOf(4, 2, 3, 3, 4, 4, 4)
+val hobi: MutableList<String> = mutableListOf("Futsal", "Nonton", "Tidur", "Dll")
+var jenisKelamin: String = "L"
+
+//deklarasi variabel
+lateinit var namaDepan: String
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
+
+    //inisialisasi
+    namaDepan = "Muhammad"
+
+    println("Nama Lengkap           : $namaDepan $nama")
+    println("Usia                   : $usia")
+    println("Berat Badan            : $beratBadan")
+    println("Tinggi Badan           : $tinggiBadan")
+    println("Seorang pelajar?       : $isStudent")
+    println("Pekerjaan              : ${Arrays.toString(pekerjaan)}")
+    println("Nilai semester akhir   : ${nilaiPerSemester.last()}")
+    println("Hobi                   : ${hobi[1]}")
+    println("Jenis Kelamin          : $jenisKelamin")
+    println("Berat badan ideal      : ${tinggiBadan-100}")
+}
