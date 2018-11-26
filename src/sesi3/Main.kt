6288383cc55a3ce794215d14b8888fa8f29ee307
ilhamsuaib <ilhamsuaib10@gmail.com>
2018-11-26 18:43:07 +0700

    sesi 4 : function & condition

--- src/sesi3/Main.kt
+++ src/sesi3/Main.kt
@@ -2,6 +2,13 @@ package sesi3
 
 import java.util.*
 
+/**
+ * push to master :
+ * 1. git add .
+ * 2. git commit -m "your message here"
+ * 3. git push origin master
+ * */
+
 fun main(args: Array<String>) {
     //blokArray()
     //blokList()
